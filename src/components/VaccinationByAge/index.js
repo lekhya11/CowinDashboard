3af3bed45bbbbe4862d7f0c845526a93eff3f969
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-09-05 14:34:42 +0530

    bar chart from recharts

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,1 +1,53 @@
-// Write your code here
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import './index.css'
+
+const data = [
+  {
+    count: 809680,
+    language: 'Telugu',
+  },
+  {
+    count: 4555697,
+    language: 'Hindi',
+  },
+  {
+    count: 12345657,
+    language: 'English',
+  },
+]
+
+const VaccinationByAge = () => {
+  const dataValues = data
+  return (
+    <div className="age-container">
+      <h1 className="heading">Vaccination by Age</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            cx="70%"
+            cy="40%"
+            data={dataValues}
+            startAngle={0}
+            endAngle={360}
+            innerRadius="40%"
+            outerRadius="70%"
+            dataKey="count"
+          >
+            <Cell name="Telugu" fill="#fecba6" />
+            <Cell name="Hindi" fill="#b3d23f" />
+            <Cell name="English" fill="#a44c9e" />
+          </Pie>
+          <Legend
+            iconType="circle"
+            layout="vertical"
+            verticalAlign="middle"
+            align="right"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default VaccinationByAge
+
