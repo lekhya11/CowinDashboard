3a6d2cbb36ddb70617064abd1716c3bde644e2e7
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-09-05 14:37:26 +0530

    Bar chart

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -1,1 +1,80 @@
-// Write your code here
+import './index.css'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+
+const data = [
+  {
+    group_name: 'Group A',
+    boys: 200,
+    girls: 400,
+  },
+  {
+    group_name: 'Group B',
+    boys: 3000,
+    girls: 500,
+  },
+  {
+    group_name: 'Group C',
+    boys: 1000,
+    girls: 1500,
+  },
+  {
+    group_name: 'Group D',
+    boys: 700,
+    girls: 1200,
+  },
+]
+
+const VaccinationCoverage = () => {
+  const DataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+
+  return (
+    <div className="mini-container">
+      <h1 className="main-heading">Vaccination Coverage </h1>
+      <ResponsiveContainer width="100%" height={500}>
+        <BarChart
+          data={data}
+          margin={{
+            top: 5,
+          }}
+        >
+          <XAxis
+            dataKey="group_name"
+            tick={{
+              stroke: 'gray',
+              strokeWidth: 1,
+            }}
+          />
+          <YAxis
+            tickFormatter={DataFormatter}
+            tick={{
+              stroke: 'gray',
+              strokeWidth: 0,
+            }}
+          />
+          <Legend
+            wrapperStyle={{
+              padding: 30,
+            }}
+          />
+          <Bar dataKey="boys" name="Boys" fill="#5a8dee" barSize="20%" />
+          <Bar dataKey="girls" name="Girls" fill="#f54394" barSize="20%" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default VaccinationCoverage
+
