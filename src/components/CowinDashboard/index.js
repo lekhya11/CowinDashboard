568f3a77404adaac86e08831f3c96c1e459c026f
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-09-05 14:32:34 +0530

    class component where fetching API

--- src/components/CowinDashboard/index.js
+++ src/components/CowinDashboard/index.js
@@ -1,52 +1,36 @@
 import {Component} from 'react'
 
-import Loader from 'react-loader-spinner'
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import VaccinationCoverage from '../VaccinationCoverage'
 
 import './index.css'
+import VaccinationByAge from '../VaccinationByAge'
+import VaccinationByGender from '../VaccinationByGender'
 
-class CowinDashboard extends Component {
-  state = {
-    isLoading: true,
-  }
-
-  componentDidMount() {
-    this.getVaccinationData()
-  }
-
-  convertCoverage = covData => {
-    console.log(covData)
-  }
-
-  getVaccinationData = async () => {
-    const response = await fetch('https://apis.ccbp.in/covid-vaccination-data')
-    const data = await response.json()
-    console.log(data)
-    this.convertCoverage(data.last_7_days_vaccination)
-  }
-
-  renderMainPage = () => {
-    const {isLoading} = this.state
-
-    return isLoading ? (
-      <Loader type="Audio" color="#00BFFF" height={50} width={50} />
-    ) : (
+class CowinDashBoard extends Component {
+  render() {
+    return (
       <div className="container">
-        <h1 className="logo-name">
+        <nav className="logo-container">
           <img
             src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
             alt="website logo"
-            className="co-win-logo"
+            className="logo"
           />
-          CO-WIN
-        </h1>
+          <h1 className="logo-name">CO-WIN </h1>
+        </nav>
+        <h1 className="main-heading">CoWIN Vaccination in INDIA</h1>
+        <div>
+          <VaccinationCoverage />
+        </div>
+        <div>
+          <VaccinationByAge />
+        </div>
+        <div>
+          <VaccinationByGender />
+        </div>
       </div>
     )
   }
-
-  render() {
-    return <div>{this.renderMainPage}</div>
-  }
 }
 
-export default CowinDashboard
+export default CowinDashBoard
