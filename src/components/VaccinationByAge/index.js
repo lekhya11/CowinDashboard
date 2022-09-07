862054a0f41bdee2b2e44805a93cf29e8214d657
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-09-07 10:55:15 +0530

    Update index.js

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,53 +1,37 @@
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 import './index.css'
 
-const data = [
-  {
-    count: 809680,
-    language: 'Telugu',
-  },
-  {
-    count: 4555697,
-    language: 'Hindi',
-  },
-  {
-    count: 12345657,
-    language: 'English',
-  },
-]
+const VaccinationByAge = props => {
+  const {vaccinationByAgeDetails} = props
 
-const VaccinationByAge = () => {
-  const dataValues = data
   return (
     <div className="age-container">
       <h1 className="heading">Vaccination by Age</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="70%"
-            cy="40%"
-            data={dataValues}
-            startAngle={0}
-            endAngle={360}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="Telugu" fill="#fecba6" />
-            <Cell name="Hindi" fill="#b3d23f" />
-            <Cell name="English" fill="#a44c9e" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="vertical"
-            verticalAlign="middle"
-            align="right"
-          />
-        </PieChart>
-      </ResponsiveContainer>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="30%"
+          data={vaccinationByAgeDetails}
+          startAngle={0}
+          endAngle={360}
+          innerRadius="0%"
+          outerRadius="60%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#2cc6c6" />
+          <Cell name="44-60" fill="#a3df9f" />
+          <Cell name="Above 60" fill="#64c2c6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+          wrapperStyle={{fontSize: 12}}
+        />
+      </PieChart>
     </div>
   )
 }
 
 export default VaccinationByAge
-
