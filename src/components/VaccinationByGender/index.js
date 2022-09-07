d17e8fa26b0ccb2a4ca7bdcbe5c224de3ba6ea90
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-09-07 10:53:34 +0530

    getting details from props

--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,51 +1,37 @@
 
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 import './index.css'
 
-const data = [
-  {
-    count: 809680,
-    language: 'Telugu',
-  },
-  {
-    count: 4555697,
-    language: 'Hindi',
-  },
-  {
-    count: 12345657,
-    language: 'English',
-  },
-]
+const VaccinationByGender = props => {
+  const vaccinationByGenderDetails = props
 
-const VaccinationByGender = () => {
-  const dataValues = data
   return (
     <div className="gender-container">
       <h1 className="heading"> Vaccination by Gender</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="70%"
-            cy="40%"
-            data={dataValues}
-            startAngle={0}
-            endAngle={360}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="Telugu" fill="#fecba6" />
-            <Cell name="Hindi" fill="#b3d23f" />
-            <Cell name="English" fill="#a44c9e" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="vertical"
-            verticalAlign="middle"
-            align="right"
-          />
-        </PieChart>
-      </ResponsiveContainer>
+
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="60%"
+          data={vaccinationByGenderDetails}
+          startAngle={180}
+          endAngle={0}
+          innerRadius="30%"
+          outerRadius="60%"
+          dataKey="count"
+        >
+          <Cell name="Male" fill="#f54394" />
+          <Cell name="Female" fill="#5a8dee" />
+          <Cell name="Others" fill="#2cc6c6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+          wrapperStyle={{fontSize: 12}}
+        />
+      </PieChart>
     </div>
   )
 }
