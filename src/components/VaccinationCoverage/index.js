d59f13f578c4ed777974afc93a1b72c6e1b85989
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-09-09 10:40:03 +0530

    Update index.js

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -9,7 +9,7 @@ const VaccinationCoverage = props => {
     return number.toString()
   }
 
-  const {VaccinationCoverageDetails} = props
+  const {vaccinationCoverageDetails} = props
 
   return (
     <div className="mini-container">
@@ -18,7 +18,7 @@ const VaccinationCoverage = props => {
       <BarChart
         width={900}
         height={400}
-        data={VaccinationCoverageDetails}
+        data={vaccinationCoverageDetails}
         margin={{
           top: 5,
         }}
